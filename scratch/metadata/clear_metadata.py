1ad5f151da32b95b6d4c77349fb093e25627a849
topher097 <cmendres400@gmail.com>
2019-10-19 20:22:37 -0500

    metadata finish

--- scratch/metadata/clear_metadata.py
+++ scratch/metadata/clear_metadata.py
@@ -4,6 +4,7 @@ import pandas as pd
 import comtypes.client
 import time
 import shutil
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 class clearMetadata:
@@ -17,11 +18,27 @@ class clearMetadata:
         self.current_dir = os.getcwd()
         self.word_counter = 0
         self.excel_counter = 0
-        self.word = comtypes.client.CreateObject('Word.Application')         # Open word
         self.root = ''
         self.file_basename = ''
         self.file_ext = ''
         self.file = ''
+        self.new_pdf_metadata = {u'/Author': u'Anonymous',
+                                 u'/Creator': u'Anonymous',
+                                 u'/Producer': u'Anonymous',
+                                 u'/Title': u'Anonymous'
+                                 }
+        self.info_text = ''
+        self.legpath = os.path.join(self.current_dir, r'AE483_Lab2_Materials_LEGACY.zip')
+
+    # Initialize test folder from legacy (for testing purposes, unzip folder)
+    def initialize(self):
+        try:
+            shutil.rmtree(self.dpath)
+        except Exception:
+            pass
+        with zipfile.ZipFile(self.legpath, 'r') as zip_ref:
+            zip_ref.extractall(self.current_dir)
+        os.rename(os.path.basename(self.legpath).replace('.zip', ''), os.path.basename(self.dpath))
 
     # Unzip all files in directory (if any)
     def unZipFiles(self):
@@ -40,13 +57,17 @@ class clearMetadata:
         for root, dpath, files in os.walk(self.dpath):
             for file in files:
                 file_basename, file_extension = os.path.splitext(os.path.basename(file))
-                if file_extension in self.clear_ext and '_COPY' not in file_basename:
-                    print(f'"{file}" may have metadata')
+                if file_extension in self.clear_ext:
+                    print_string = f'"{file}" may have metadata'
+                    print(print_string)
+                    self.info_text += str(print_string + '\n')
                     file_full_path = os.path.join(root, file)
                     self.clean_list.append(file_full_path)
 
-    # Clear metadata by re-writing or printing as pdf
+    # Clean file based on extension
     def cleanFiles(self):
+        self.info_text += '\n\nMetrics:\n'
+        word = comtypes.client.CreateObject('Word.Application')  # Open Word Application
         for file in self.clean_list:
             self.file = file
             self.file_basename, self.file_ext = os.path.splitext(os.path.basename(file))
@@ -55,51 +76,92 @@ class clearMetadata:
             # Clean the excel file by converting to csv
             if self.file_ext in self.excel_ext:
                 clearMetadata.cleanExcel(self)
-
-            # Clean word file by printing to pdf
+            # Clean the word file by saving as PDF
             if self.file_ext in self.word_ext:
-                clearMetadata.cleanWord(self)
-        self.word.Quit()
+                clearMetadata.cleanWord(self, word)
+        word.Quit()     # Quit Word Application
 
-    def cleanWord(self):
-        self.word_counter += 1
-        start_time = time.time()
+    # Clean word file by saving as pdf and changing metadata
+    def cleanWord(self, word):
+        try:
+            self.word_counter += 1
+            start_time = time.time()
 
-        pdf_file_temp = 'temp.PDF'
-        pdf_file = self.file_basename + f'_wordconvert{self.word_counter}.PDF'      # New name for file
-        doc = self.word.Documents.Open(self.file)  # Open word file
-        doc.SaveAs(os.path.join(self.root, pdf_file_temp), FileFormat=17)           # Save as PDF
-        doc.Close()                                                                 # Close word file
-        shutil.copyfile(os.path.join(self.root, pdf_file_temp), os.path.join(self.root, pdf_file))
-        os.remove(pdf_file_temp)                                                    # Remove the temp file
-        end_time = time.time()
-        print(f'Converting "{self.file_basename + self.file_ext}" to "{pdf_file}" in {round((end_time - start_time) * 1000, 4)} ms')
+            # Save word file as a PDf
+            pdf_file_temp = 'temp.PDF'
+            pdf_file = self.file_basename + f'_wordconvert{self.word_counter}.PDF'      # New name for PDF file
+            doc = word.Documents.Open(self.file)                                        # Open word file
+            doc.SaveAs(os.path.join(self.root, pdf_file_temp), FileFormat=17)           # Save as PDF
+            doc.Close()                                                                 # Close word file
+
+            # Modify PDF metadata
+            with open(pdf_file_temp, 'rb') as p:
+                pdf = PdfFileReader(p)
+                writer = PdfFileWriter()
+                for page in range(pdf.getNumPages()):
+                    writer.addPage(pdf.getPage(page))
+                writer.addMetadata(self.new_pdf_metadata)
+                with open(pdf_file, 'wb') as f2:
+                    writer.write(f2)
+                p.close()
+
+            os.remove(pdf_file_temp)    # Remove temp pdf file
+            os.remove(self.file)
+            end_time = time.time()
+            total_time = end_time - start_time
+            print_string = f'Converted "{self.file_basename + self.file_ext}" to "{pdf_file}" ' \
+                           f'in {round(total_time * 1000, 4)} ms'
+            print(print_string)
+            self.info_text += str(print_string + '\n')
+        except Exception as e:
+            print_string = f'Cound not clean "{self.file_basename + self.file_ext}", error: {str(e)}'
+            print(print_string)
+            self.info_text += '*' + str(print_string) + '\n'
 
     # Clean Excel file by converting to csv file(s)
     def cleanExcel(self):
-        self.excel_counter += 1
-        excel = pd.ExcelFile(self.file)                                     # Open Excel file
-        sheet_names = excel.sheet_names                                     # Get list of names of sheets
-        for sheet in sheet_names:
-            start_time = time.time()
-            excel_data = excel.parse(sheet)                                 # Parse Excel data
-            if not excel_data.empty:
-                csv_file = self.file_basename + '_' + sheet + f'{self.excel_counter}.csv'
-                csv_path = os.path.join(self.root, csv_file)
-                if os.path.exists(csv_path):
-                    os.remove(csv_path)
-                excel_data.to_csv(csv_path, sep=',', encoding='utf-8', index=False)
-                end_time = time.time()
-                print(f'Converting "{self.file_basename + self.file_ext}" to "{csv_file}" in {round((end_time - start_time) * 1000, 4)} ms')
+        try:
+            self.excel_counter += 1
+            excel = pd.ExcelFile(self.file)                                     # Open Excel file
+            sheet_names = excel.sheet_names                                     # Get list of names of sheets
+            for sheet in sheet_names:
+                start_time = time.time()
+                excel_data = excel.parse(sheet)                                 # Parse Excel data
+                if not excel_data.empty:
+                    csv_file = self.file_basename + '_' + sheet + f'_{self.excel_counter}.csv'
+                    csv_path = os.path.join(self.root, csv_file)
+                    excel_data.to_csv(csv_path, sep=',', encoding='utf-8', index=False)
+                    end_time = time.time()
+                    total_time = end_time - start_time
+                    print_string = f'Converted "{self.file_basename + self.file_ext}" to "{csv_file}" in ' \
+                                   f'{round(total_time * 1000, 4)} ms'
+                    print(print_string)
+                    self.info_text += str(print_string + '\n')
+            os.remove(self.file)
+        except Exception as e:
+            print_string = f'Cound not clean "{self.file_basename + self.file_ext}", error: {str(e)}'
+            print(print_string)
+            self.info_text += '*' + str(print_string) + '\n'
+
+    # Write the info file for reviewers/diagnostics
+    def writeInfoFile(self):
+        info_file_name = '000_CLEAN_INFO.txt'
+        with open(os.path.join(self.dpath, info_file_name), 'wt') as text:
+            text.write(self.info_text)
+        text.close()
 
 
 # Run the cleaner
 if __name__ == '__main__':
     # Specify directory which you want to search
-    directory_path = r'C:\Python\SeniorAE\scratch\metadata\AE483_Lab2_Materials'
+    directory_path = r'C:\Users\TOPHER-LAPTOP\Desktop\SeniorAE\scratch\metadata\AE483_Lab2_Materials_LEGACY'
+    #directory_path = r'C:\\path\to\folder\of\files'
 
     # Clean files
     clean = clearMetadata(directory_path)
+    clean.initialize()
     clean.unZipFiles()
     clean.findFiles()
     clean.cleanFiles()
+    clean.writeInfoFile()
+
