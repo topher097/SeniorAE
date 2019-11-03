3d3e32478640d372fee96463a57dd04a759606f3
topher097 <cmendres400@gmail.com>
2019-11-02 23:57:09 -0500

    AE416 hw3 - update

--- AE416/HW_3/AE416_hw3.py
+++ AE416/HW_3/AE416_hw3.py
@@ -30,29 +30,22 @@ problem3:
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import Symbol
+import os
 import scipy.interpolate
+from math import sqrt
 
 
 
-class vortexPanels:
+class vortexPanels():
 
     # Initialize variables
     def __init__(self):
-        self.master_panels = {}        # Dict of panel and their node coordinates, length, vortex location,
-                                                # and control point location
-        self.camber_line_points = []        # Define the camber line points in (x, y) sets
-        self.panel_num = 40                 # Number of panels in camberline
-        self.max_length = .05               # Set max panel length (by percentage of chord length)
-        self.min_length = .01               # Ser min panel length (by percentage of chord length)
-
+        self.master_panels = {}             # Dict of panel and their node coordinates, length, vortex location,
+                                            # and control point location
+        self.panel_num = 10                 # Number of panels in camberline
         self.v_inf = 0                      # Freestream velocity scalar (m/s)
         self.alpha = 0                      # Angle of attack (degrees)
         self.chord_length = 1               # Chord length of airfoil (m)
-        self.camber_arclength = 0           # Arc length of camberline [m]
-
-    # Use camber line points and number of panels to create panels, store node points and panel number in dict
-    def createPanels(self):
-        pass
 
     # Solve the lumped vortex method given master coord dict, v_inf, and alpha
     def solvePanele(self):
@@ -61,18 +54,18 @@ class vortexPanels:
     # Compute problem 1
     def problemOne(self):
         # Define the camber line in (x, y) point sets
+        x = np.linspace(0, 1, self.panel_num)
+        y = np.zeros(len(x))
 
-        x = np.linspace(0, 1, self.panel)
-        self.camber_line_points = []
 
-        # Create panels
-        vortexPanels.createPanels(self)
+
         # Run panel solver
         vortexPanels.solvePanele(self)
 
     # Compute problem 2
-    def problemTwo(self):
-        # Define the camber line in (x, y) point sets
+    def problemTwo(self, panel_num):
+        self.panel_num = panel_num
+        # Define the camber line in (x, y) point sets, based on number of panels wanted and
         m = 0.25025
         k = 15.957
         x_i = np.linspace(0, 1, 100)       # Create array of x values to interpolate
@@ -85,23 +78,82 @@ class vortexPanels:
                 y = (k*m**3)/6 * (1-x)
             y_i.append(y)
 
-        # Create interpolation function
+        # Create interpolation function and x-y pairs for plotting
         z = scipy.interpolate.CubicSpline(x_i, y_i)
+        x = np.linspace(0, 1, 1000)
+        y = z(x)
 
-        # Compute the length of the camberline by calculating the length between many nodes along camberline
-        x_r = np.linspace(0, 1, 1000)
-        for i in range(0, len(x_r)-1):
-            x1 = x_r[i]
-            x2 = x_r[i+1]
-            y1 = z(x1)
-            y2 = z(x2)
-            self.camber_arclength += np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        # Discretize points along the curve
+        radius = (x_i.max() - x_i.min()) / 2        # Get radius of discretization circle
+        x_center = (x_i.max() + x_i.min()) / 2      # Get center of circle (between endpoints of camberline)
+        x_circle = (x_center + radius * np.cos(np.linspace(np.pi, 0,  self.panel_num + 1))).tolist()    # X-coords of circle
 
-        # Compute the
-        self.camber_line_points = []
+        # Get y coordinates of the panel nodes, length of panel, location of control points and vortex
+        for i in range(self.panel_num):
+            x1 = x_circle[i]
+            x2 = x_circle[i+1]
+            y1 = float(z(x1))
+            y2 = float(z(x2))
+            node1 = (x1, y1)
+            node2 = (x2, y2)
+            length = sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-        # Create panels
-        vortexPanels.createPanels(self)
+            # Get location of control points and vortex locations along panel
+            v = np.array([x2-x1, y2-y1])
+            u = v/length
+            vortex_point = np.array(node1) + 0.25*length*u
+            control_point = np.array(node1) + 0.75*length*u
+
+            # Write data to dictionary
+            self.master_panels[i] = {'n1': node1,
+                                     'n2': node2,
+                                     'l': length,
+                                     'c_p': control_point.tolist(),
+                                     'v_p': vortex_point.tolist()
+                                     }
+
+        # Plot the points of the discretized camberline and control and vortex locations
+        cp_plot_x, cp_plot_y = [], []
+        vp_plot_x, vp_plot_y = [], []
+        plot_x1, plot_y1 = [], []
+        plot_x2, plot_y2 = [], []
+        for panel_num in self.master_panels.keys():
+            panel = self.master_panels[panel_num]
+            node1 = panel['n1']
+            node2 = panel['n2']
+            plot_x1.append(node1[0])
+            plot_y1.append(node1[1])
+            plot_x2.append(node2[0])
+            plot_y2.append(node2[1])
+
+            c_p = panel['c_p']
+            cp_plot_x.append(c_p[0])
+            cp_plot_y.append(c_p[1])
+            v_p = panel['v_p']
+            vp_plot_x.append(v_p[0])
+            vp_plot_y.append(v_p[1])
+
+        camber_plot = plt.figure(figsize=(12, 6))
+        camber_plot.subplots_adjust(hspace=.5)
+        cp = camber_plot.add_subplot(1, 1, 1)
+        plt.xlim(-.25, 1.25)
+        plt.ylim(-.25, .25)
+        plt.gca().set_aspect('equal', adjustable='box')
+        cp.set_title(f'Panels, Control Points, and Vortex Locations - {self.panel_num} Panels')
+        cp.set_xlabel('x/c')
+        cp.set_ylabel('y/c')
+        cp.grid(linewidth=0.5, linestyle='--', color='grey')
+        cp.plot(x, y, 'b', linewidth=1, label='camberline')                         # Camberline
+        cp.plot(plot_x1, plot_y1, color='k', label='panels', linewidth=.75)         # Panels
+        cp.plot(plot_x2, plot_y2, color='k', linewidth=.75)                         # Last panel
+        cp.scatter(cp_plot_x, cp_plot_y, color='g', marker='x', s=50, label='control points')    # Control point
+        cp.scatter(vp_plot_x, vp_plot_y, color='y', marker='+', s=50, label='vortex locations')  # Vortex location
+        cp.scatter(plot_x1, plot_y1, color='r', marker='o', s=25)       # Node 1
+        cp.scatter(plot_x2, plot_y2, color='r', marker='o', s=25)       # Node 2
+        cp.legend(loc='upper left', fontsize=12)
+        camber_plot.savefig(os.path.join(os.getcwd(), f'plots\\P2_{self.panel_num}_panels'))
+        plt.draw()
+
         # Run panel solver
         vortexPanels.solvePanele(self)
 
@@ -110,6 +162,11 @@ if __name__ == '__main__':
     # Initialize class
     panel = vortexPanels()
 
-    # Run Problems
+    # Run Problem1
     panel.problemOne()
-    panel.problemTwo()
+
+    # Run Problem2
+    for i in [10, 20, 30, 40]:
+        num_panels = i
+        panel.problemTwo(num_panels)
+    plt.show()
