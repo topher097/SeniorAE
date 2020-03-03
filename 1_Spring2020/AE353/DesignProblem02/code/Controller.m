42a413cda4c467cb1cc9972fef02e4a0911938ca
topher097 <52308267+topher097@users.noreply.github.com>
2020-03-03 00:56:32 -0600

    353 dp2

--- 1_Spring2020/AE353/DesignProblem02/code/Controller.m
+++ 1_Spring2020/AE353/DesignProblem02/code/Controller.m
@@ -34,11 +34,30 @@ end
 %
 
 function [data] = initControlSystem(parameters, data)
+data.tau_max = 100;
+syms tau theta zeta thetadot zetadot real
+f = parameters.symEOM.f;
 
-%
-% Here is a good place to initialize things...
-%
+x = [zeta; zetadot; theta; thetadot];
+xdot = [zetadot; f(1); thetadot; f(2)];
+u = [tau];
 
+theta_e = 0;
+zeta_e = 0;
+thetadot_e = 0;
+zetadot_e = 0;
+tau_e = 0;
+
+% Double turns the symbolic expression into a floating point matrix
+data.A = double(subs(jacobian(xdot, x), [zeta; theta; zetadot; thetadot; tau], [zeta_e; theta_e; zetadot_e; thetadot_e; tau_e]));
+data.B = double(subs(jacobian(xdot, u), [zeta; theta; zetadot; thetadot; tau], [zeta_e; theta_e; zetadot_e; thetadot_e; tau_e]));
+data.C = [1 0 0 0];
+    
+Q = diag([25, 5, 25, 5]);
+R = diag([10]);
+[K, P] = lqr(data.A, data.B, Q, R);
+data.K = K;
+data.kRef = -1/(data.C*inv(data.A - data.B * data.K)*data.B);
 end
 
 %
@@ -47,7 +66,19 @@ end
 %
 
 function [actuators, data] = runControlSystem(sensors, references, parameters, data)
-actuators.tau = 0;
+% Use the state-space model for control
+x = [sensors.zeta; sensors.zetadot; sensors.theta; sensors.thetadot];
+r = [.25*sensors.t; 0; 0; 0];
+u = -data.K * x + data.kRef * r;
+% Set the torque applied to the wheel
+tau = u(1);
+if tau < -data.tau_max
+    tau = -data.tau_max;
+end
+if tau > data.tau_max
+    tau = data.tau_max;
+end
+actuators.tau = tau;
 end
 
 
--- 1_Spring2020/AE353/DesignProblem02/code/GenerateResults.m
+++ 1_Spring2020/AE353/DesignProblem02/code/GenerateResults.m
@@ -2,18 +2,18 @@ clear all;
 clc;
 
 % Define initial conditions and parameters for controller
-tstop = 5;
+tstop = 10;
 zeta_o = 0;
 theta_o = 0;
 zetadot_o = 0;
 thetadot_o = 0;
-initial = [zeta_o, theta_o, zetadot_o, thetadot_o];
+initial = [zeta_o; theta_o; zetadot_o; thetadot_o];
 datafile = 'data.mat';
-flat_bool = true;       % Flat ground
-disp_bool = true:       % Display the simulation
+flat_bool = false;       % Flat ground
+disp_bool = true;       % Display the simulation 
 
 % Define file to save data to
 csvfile = 'data.csv';
 
 % Run the design problem 2
-DesignProblem02('Controller','initial',initial,'tstop',tstop,'flatground',flat_bool,'display',disp_bool);
+DesignProblem02('Controller','initial',initial,'tstop',tstop,'diagnostics',true,'flatground',flat_bool,'display',disp_bool);
